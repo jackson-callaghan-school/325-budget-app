c2c233221dee7413b8b5edb7b1290377a94ea337
Deniz <deniz.c.guler@gmail.com>
2020-11-11 13:46:59 -0500

    added pagnation to the swiper

--- budget-app/src/App.js
+++ budget-app/src/App.js
@@ -1,15 +1,22 @@
 import { Card } from './components/card/Card';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css'
+import 'swiper/swiper-bundle.min.css'
 import './App.css';
+import SwiperCore, { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 function App() {
+  SwiperCore.use([Pagination]);
+
   return (
     <div className="App">
       <div className='container'>
         <Swiper
           spaceBetween={50}
           slidesPerView={1}
+          pagination={{
+            el: '.swiper-pagnation',
+            clickable: true,
+          }}
         >
           <SwiperSlide>
             <Card title='test card'>
@@ -21,6 +28,7 @@ function App() {
               <div className="center">hello again!</div>
             </Card>
           </SwiperSlide>
+          <div className='swiper-pagnation'></div>
         </Swiper>
       </div>
     </div>
