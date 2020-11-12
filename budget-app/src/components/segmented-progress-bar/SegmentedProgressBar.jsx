6a2f0a438bb4bf458d10b39f64f1cb69adb77af4
Deniz <deniz.c.guler@gmail.com>
2020-11-12 14:19:01 -0500

    added segmented progress bar

---
+++ budget-app/src/components/segmented-progress-bar/SegmentedProgressBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './SegmentedProgressBar.css';
+
+/**
+ * Expects a data array passed to with each object following:
+ * item : {
+ *    name: string, name of the item
+ *    value: number, percentage of the bucket/bar it take up
+ *    color?: string, hex code for the color of this object (defaults to #22a6b3)
+ * }
+ * 
+ */
+
+/**
+ * Segmented progress bar component
+ */
+export const SegmentedProgressBar = ({ data, title, className }) => {
+  const bars = data && data.length && data.map((item, i) => {
+    // wont display is value is less than 0 (can be changed)
+    if (item.value > 0) {
+      return (
+        <div className='bar' key={i} style={{ backgroundColor: item.color || '#22a6b3', width: item.value + '%' }} />
+      )
+    }
+  })
+
+
+  return (
+    <div className={'segmented-progress-bar ' + className}>
+      {title && <div className="title">{title}</div>}
+      <div className='bars'>
+        {bars === '' ? '' : bars}
+      </div>
+    </div>
+  )
+}
